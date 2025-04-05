4795ac0464c4a888a810e83886762439cbcafeae
rojojun <zoloman316@gmail.com>
2025-04-05 19:41:56 +0900

    ProxyFactory #1

--- src/main/java/com/rojojun/springadvanced/SpringAdvancedApplication.java
+++ src/main/java/com/rojojun/springadvanced/SpringAdvancedApplication.java
@@ -1,7 +1,5 @@
 package com.rojojun.springadvanced;
 
-import com.rojojun.springadvanced.app.proxy.config.v1_proxy.ConcreteProxyConfig;
-import com.rojojun.springadvanced.app.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import com.rojojun.springadvanced.app.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import com.rojojun.springadvanced.trace.logtrace.LogTrace;
 import com.rojojun.springadvanced.trace.logtrace.ThreadLocalLogTrace;
---
+++ src/test/java/com/rojojun/springadvanced/proxyfactory/ProxyFactoryTest.java
@@ -0,0 +1,35 @@
+package com.rojojun.springadvanced.proxyfactory;
+
+import com.rojojun.springadvanced.common.advice.TimeAdvice;
+import com.rojojun.springadvanced.proxy.common.service.ServiceImpl;
+import com.rojojun.springadvanced.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@Slf4j
+public class ProxyFactoryTest {
+
+    @Test
+    @DisplayName("인터페이스가 있으면 JDK 동적 프록시 사용")
+    void interfaceProxy() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("targetClass = {}", target.getClass());
+        log.info("proxyClass = {}", proxy.getClass());
+
+        proxy.save();
+
+        // AOP 프록시 (ProxyFactory)를 사용할 때만 사용 가능
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+}
